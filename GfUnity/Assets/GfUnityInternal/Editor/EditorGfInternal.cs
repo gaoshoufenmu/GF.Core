a47ab511a6868a08fdf785c17cf952262e023912
lion <xiaolige945@vip.qq.com>
2016-03-03 00:25:54 +0800

    项目目录结构调整。

---
+++ GfUnity/Assets/GfUnityInternal/Editor/EditorGfInternal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml;
+
+public class EditorGfInternal : EditorWindow
+{
+    //-------------------------------------------------------------------------
+    [MenuItem("Gf/导出GfUnity.unitypackage")]
+    static void exportGf4UnityPackage()
+    {
+        string[] arr_assetpathname = new string[2];
+        arr_assetpathname[0] = "Assets/GfUnity";
+        arr_assetpathname[1] = "Assets/Plugins";
+        AssetDatabase.ExportPackage(arr_assetpathname, "GfUnity.unitypackage", ExportPackageOptions.Recurse);
+
+        Debug.Log("Export GfUnity.unitypackage Finished!");
+    }
+}
---
+++ _CopyShareLib.bat
@@ -0,0 +1,10 @@
+@echo off
+
+rd /s /q .\Output\Bin\Debug\
+
+xcopy /e /s /f /i /r /y /k .\ShareLib\Orleans\*.* .\Output\Bin\Debug\
+
+@echo Gf done
+
+pause
+echo on
